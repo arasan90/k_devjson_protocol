17a3b05aed1f5e24ce80a66217bebdb626865f83
arasan90 <massimilianoianniello@gmail.com>
2026-07-30 11:06:17 +0200

    Update k_devjson_protocol_mock.c

--- mock/k_devjson_protocol_mock.c
+++ mock/k_devjson_protocol_mock.c
@@ -14,5 +14,5 @@
 /* Variable ------------------------------------------------------------------*/
 /* Function Definition -------------------------------------------------------*/
 DEFINE_FAKE_VOID_FUNC(k_devjson_protocol_register_callback, k_devjson_protocol_callback_t)
-DEFINE_FAKE_VALUE_FUNC(k_devjson_protocol_parse_status_t, k_devjson_protocol_parse, const char *, char *, size_t)
+DEFINE_FAKE_VALUE_FUNC(k_devjson_protocol_parse_status_t, k_devjson_protocol_parse, const char *, char *, size_t, size_t)
 DEFINE_FAKE_VOID_FUNC(k_devjson_protocol_add_response, cJSON *, const char *, k_devjson_protocol_value_t, k_devjson_protocol_value_type_t)
